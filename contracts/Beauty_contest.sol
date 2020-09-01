47314170ce93ba23252ea306979838bbb153292a
nknavkal <nknavkal@gmail.com>
2020-09-01 11:31:03 -0500

    added solhint, integrated safemath, torganzing it as structs

--- contracts/Beauty_contest.sol
+++ contracts/Beauty_contest.sol
@@ -1,36 +1,50 @@
 pragma solidity >=0.4.22 <0.8.0;
 
+import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
+
 contract Beauty_contest {
+	using SafeMath for uint;
 
 	struct bet {
 		uint amount;
 		uint8 choice;
 	}
 
+	struct choice {
+		mapping (address => uint) betAmounts;
+		uint pot;
+	}
 
-	mapping (address => uint) betValues;
-	mapping (address => uint8) betChoices;
+	// choice choices[5];
 
+	// mapping (address => bet) bets;
+	mapping (address => bet) bets;
+
+	string choice0;
 	string choice1;
 	string choice2;
 	string choice3;
 	string choice4;
-	string choice5;
 
-	constructor(string _choice1, string _choice2, string _choice3, string _choice4, string _choice5) public {
+	constructor(string _question, string _choice0, string _choice1, string _choice2, 
+		string _choice3, string _choice4) public {
+		question = _question;
+		choice0 = _choice0;
 		choice1 = _choice1;
 		choice2 = _choice2;
 		choice3 = _choice3;
 		choice4 = _choice4;
-		choice5 = _choice5;
 	}
 
 	function makeBet(uint8 choice) payable {
-		require(msg.value < 5 * 1000000000000000000, "No bets over 5 eth please!");
+		require(msg.value < mul(5,1000000000000000000), "No bets over 5 eth please!");
+		choices[choice].betAmounts[msg.sender] = msg.value;
+		choices[choice].pot = add(choices[choice].pot, msg.value);
 	}
 
+//view
 	function getMyBet() returns (string choice, unit value){
-		return(betValues[msg.sender], betChoices[msg.sender])
+		return(bets[msg.sender].amount, bets[msg.sender].choice);
 	}
 
 	function payout() {
